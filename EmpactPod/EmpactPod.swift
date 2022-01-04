56806cbf247879714a899c55c15c544fa1a13f23
ismailsafadi <ismailsafadi@gmail.com>
2022-01-04 13:32:55 +0200

    Add EmpactPod.podpec

--- EmpactPod/EmpactPod.swift
+++ EmpactPod/EmpactPod.swift
@@ -5,4 +5,15 @@
 //  Created by IsmailSafadi on 04.01.2022.
 //
 
-import Foundation
+public final class EmpactPod {
+
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
