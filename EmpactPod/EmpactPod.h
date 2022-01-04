d269256ad9173f5a1a6a2695e1bae7fc988f14de
ismailsafadi <ismailsafadi@gmail.com>
2022-01-04 13:05:37 +0200

    Initial Commit

---
+++ EmpactPod/EmpactPod.h
@@ -0,0 +1,18 @@
+//
+//  EmpactPod.h
+//  EmpactPod
+//
+//  Created by IsmailSafadi on 04.01.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EmpactPod.
+FOUNDATION_EXPORT double EmpactPodVersionNumber;
+
+//! Project version string for EmpactPod.
+FOUNDATION_EXPORT const unsigned char EmpactPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmpactPod/PublicHeader.h>
+
+
