d8b30feb7618487107aec5145601586ebdfb5e97
ismailsafadi <ismailsafadi@gmail.com>
2022-01-04 13:10:20 +0200

    first commit

---
+++ EmpactPod/EmpactPod.swift
@@ -0,0 +1,8 @@
+//
+//  EmpactPod.swift
+//  EmpactPod
+//
+//  Created by IsmailSafadi on 04.01.2022.
+//
+
+import Foundation
---
+++ Shared/EmpactPodExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmpactPodExampleApp.swift
+//  Shared
+//
+//  Created by IsmailSafadi on 04.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct EmpactPodExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
